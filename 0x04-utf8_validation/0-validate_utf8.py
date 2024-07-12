cdeb0b5e2769480d9bcde2192b7d7243088c8f27
mohammedmxz <mohammedmxzk@gmail.com>
2024-07-12 01:46:12 +0000

    just added

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-Define validUTF8 function.
+UTF-8 Validation
 """
 
 
-def validUTF8(data):
+def validUTF8(data) -> bool:
     """
-    Check if the given data is a valid UTF-8 encoding.
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    chunk = 1
-
-    for item in data:
-        if chunk == 1:
-            if item >> 5 == 0b110:
-                chunk = 2
-            elif item >> 4 == 0b1110:
-                chunk = 3
-            elif item >> 3 == 0b11110:
-                chunk = 4
-            elif item >> 7 & 0b1:
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if item >> 6 != 0b10:
-                return (False)
-
-            chunk -= 1
-
-    return chunk == 1
+            if byte >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
